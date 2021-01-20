1114cd50757c5c059a3cc129df663117beb6198b
aospiridonov <aospiridonov@gmail.com>
2021-01-20 09:49:30 +0300

    add tabs screen and dummy for favorites screen

--- lib/screens/catagories_screen.dart
+++ lib/screens/catagories_screen.dart
@@ -7,27 +7,22 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeals'),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(25.0),
-        children: DUMMY_CATEGORIES
-            .map(
-              (categoryData) => CategoryItem(
-                categoryData.id,
-                categoryData.title,
-                categoryData.color,
-              ),
-            )
-            .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200.0,
-          childAspectRatio: 1.5,
-          crossAxisSpacing: 20.0,
-          mainAxisSpacing: 20.0,
-        ),
+    return GridView(
+      padding: const EdgeInsets.all(25.0),
+      children: DUMMY_CATEGORIES
+          .map(
+            (categoryData) => CategoryItem(
+              categoryData.id,
+              categoryData.title,
+              categoryData.color,
+            ),
+          )
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200.0,
+        childAspectRatio: 1.5,
+        crossAxisSpacing: 20.0,
+        mainAxisSpacing: 20.0,
       ),
     );
   }
